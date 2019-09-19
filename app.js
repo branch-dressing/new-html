739bd922b2720afb496db91a5c6ae79c805bfe11
joelpdurham <joeldur@gmail.com>
2019-09-18 20:58:21 -0700

    made a style sheet and js

---
+++ app.js
@@ -0,0 +1,25 @@
+const dog = {
+    name: 'roger',
+    color: 'purple',
+    weight: 120,
+}
+
+console.log(dog)
+
+if (dog.name === 'roger') {
+    console.log(`welcome mr. ${dog.name}`)
+} else {console.log('get outta here')};
+
+//count up to 10
+
+for (let i = 0; i<=10; i++){
+    console.log(i)
+};
+
+function showAlert() {
+    alert('you cliked the button!')
+};
+
+function logHello() {
+    console.log('Hello')
+};
